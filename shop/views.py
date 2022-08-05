cfec4af7848f1306a377606f273568a5d06d61cb
Jasur <jasur@example.com>
2022-08-05 21:07:34 +0500

    added category filter and tags

--- shop/views.py
+++ shop/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import ProductModel
+from .models import ProductModel, CategoryModel, ProductTagModel
 
 
 class ShopView(ListView):
     template_name = 'product.html'
 
     def get_queryset(self):
-        qs = ProductModel.objects.order_by('-id')
+        qs = ProductModel.objects.all()
 
         search = self.request.GET.get('search')
         if search:
             qs = qs.filter(title__icontains=search)
+
+        cat = self.request.GET.get('cat')
+        if cat:
+            qs = qs.filter(category_id=cat)
+
+        tag = self.request.GET.get('tag')
+        if tag:
+            qs = qs.filter(tags=tag)
+
         return qs
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        data = super().get_context_data()
+        data['categories'] = CategoryModel.objects.all()
+        data['tags'] = ProductTagModel.objects.all()
+        return data
 
 
 class ProductDetailView(DetailView):
